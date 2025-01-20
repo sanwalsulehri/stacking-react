9c3f4b0546e6664612d2461d9a514849842f7dae
azharmushtaq0099 <azharmushtaq344@gmail.com>
2025-01-21 01:11:31 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,29 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Stacking from './pages/Stacking';
-<<<<<<< HEAD
 import Footer from './components/Footer';
-import ReferralTable from './components/ReferralTable';
-import UserStack from './components/UserStack';
 
 
 
 
-=======
-import Dashboard from './pages/Dashboard';
->>>>>>> ef91b78a625101d98bb5e73f1bf1cbf499298b87
 const App = () => {
 
 
   return (
     <Router>
 
-    <div className="">
+    <div className="bg-[#10141B] text-white min-h-screen w-full">
         <Routes>
           <Route path="/" element={<Home />} /> 
           <Route path="/stacking" element={<Stacking />} /> 
-          <Route path="/dashboard" element={<Dashboard />} /> 
 
         </Routes>
      
       
-<<<<<<< HEAD
       <Footer />
-      <UserStack />
+
       
 
-=======
->>>>>>> ef91b78a625101d98bb5e73f1bf1cbf499298b87
     </div>
     </Router>
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SideBar from '../components/SideBar'
+import Usdt from '../components/Usdt'
 
 const Dashboard = () => {
   return (
     <div>
         <SideBar />
+        <Usdt />
     </div>
   )
 }
